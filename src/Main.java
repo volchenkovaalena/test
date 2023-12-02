1c9e04dd277995f12531947b8b7b00fc775a5999
alena <misamistake@>
2023-12-02 18:50:24 +0300

    first

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         var a = 2;
+        a = a + 2;
         System.out.println(a);
     }
  }
